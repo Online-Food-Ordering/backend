ae9b4f1e4e7482f3bc828a83bc87b61120094e3a
caspar.mayrgundter <caspar.mayrguendter@hi1.at>
2023-12-16 12:35:57 +0100

    add tests and validation for food

--- food/models.py
+++ food/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
 
 class Food(models.Model):
     name = models.CharField(max_length=100)
-    price = models.IntegerField() # price in cents
+    price = models.IntegerField(validators=[MinValueValidator(0)])  # price in cents
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,28 @@
+import pytest
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from food.models import Food
+
 
 # Create your tests here.
+
+class FoodTests(TestCase):
+
+    def setUp(self):
+        ...
+
+    def test_food_is_created(self):
+        Food.objects.create(name='Pasta', price=850)
+        pasta = Food.objects.get(name='Pasta')
+        self.assertEqual(pasta.price, 850)
+
+    def test_food_with_negative_price_throws_exception(self):
+        food = Food(name='Food', price=-500)
+        with pytest.raises(ValidationError):
+            food.full_clean()
+
+    def test_food_with_empty_name_throws_exception(self):
+        food = Food(name='', price=100)
+        with pytest.raises(ValidationError):
+            food.full_clean()
